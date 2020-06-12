a366ada12f588cfe64d4a796eaaf1eeca7e2ffe3
Joe Finney <joe@comp.lancs.ac.uk>
2020-06-12 16:17:01 +0100

    Increase TouchSensor frequency

--- inc/NRF52TouchSensor.h
+++ inc/NRF52TouchSensor.h
@@ -32,7 +32,7 @@ DEALINGS IN THE SOFTWARE.
 #include "TouchSensor.h"
 #include "NRFLowLevelTimer.h"
 
-#define NRF52_TOUCH_SENSOR_PERIOD           10000        // The period between each sensing cycle (uS)
+#define NRF52_TOUCH_SENSOR_PERIOD           1000        // The period between each sensing cycle (uS)
 #define NRF52_TOUCH_SENSE_SAMPLE_MAX        0x7FFFFFFF   // The maximum sample value returned (if the pin never raises)
 #define NRF52_TOUCH_SENSOR_PPI_CHANNEL      3           
 #define NRF52_TOUCH_SENSOR_GPIOTE_CHANNEL   0
