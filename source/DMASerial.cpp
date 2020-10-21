4f869fe7348f85a9c83063bc247909f4ff87e32b
Michal Moskal <michal@moskal.me>
2020-10-21 15:28:46 +0200

    Add sleeping send/recv

--- source/DMASerial.cpp
+++ source/DMASerial.cpp
@@ -1,4 +1,33 @@
 #include "DMASerial.h"
+#include "Event.h"
+#include "CodalFiber.h"
 
 using namespace codal;
 
+static void sendDone(void *p)
+{
+    Event(((DMASerial *)p)->id, 1000);
+}
+
+int DMASerial::send(uint8_t *data, int len)
+{
+    int r = startSend(data, len, sendDone, this);
+    if (r)
+        return r;
+    fiber_wait_for_event(this->id, 1000);
+    return DEVICE_OK;
+}
+
+static void recvDone(void *p)
+{
+    Event(((DMASerial *)p)->id, 1001);
+}
+
+int DMASerial::receive(uint8_t *data, int len)
+{
+    int r = startReceive(data, len, recvDone, this);
+    if (r)
+        return r;
+    fiber_wait_for_event(this->id, 1001);
+    return DEVICE_OK;
+}
